f53f1664f6ece8a8922263f71558294894351834
Raul Vargas <raulito@gmail.com>
2018-06-11 07:09:59 -0400

    Llenado del objeto Sentence, convertido a mayusculas

--- src/main/java/Analyzer.java
+++ src/main/java/Analyzer.java
@@ -1,11 +1,9 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Analyzer {
 	/*
@@ -14,21 +12,66 @@ public class Analyzer {
 	public static List<Sentence> readFile(String filename) {
 		Sentence sentence = new Sentence(0, null);
 
+		String cadenaLinea;
+		String cadenaPalabra;
+
 		List<String> list = new ArrayList<>();
 		try {
 			List<String> lines = Files.readAllLines(Paths.get(filename), StandardCharsets.UTF_8);
 			list.addAll(lines);
+			if (list.size() > 0) {
+				for(int i=0; i<list.size(); i++) {
+					cadenaLinea = list.get(i);
+
+					System.out.println("cadenaLinea: " + cadenaLinea);
+
+					int numTokens = 0;
+					StringTokenizer st = new StringTokenizer(cadenaLinea);
+
+					while (st.hasMoreTokens()) 	{
+						cadenaPalabra = st.nextToken();
+						numTokens++;
+
+						if (numTokens == 1) {
+							try
+							{
+								int score = Integer.parseInt(cadenaPalabra);
+
+								if (score >= -2 & score <= 2) {
+									String espacio = cadenaLinea.substring(cadenaPalabra.length(), cadenaPalabra.length() + 1);
+
+									if (espacio.charAt(0) == ' ') {
+										System.out.println("Linea valida");
+
+										sentence.score = score;
+										sentence.text = cadenaLinea.substring(cadenaPalabra.length() + 1, cadenaLinea.length()).toUpperCase();
+									} else {
+										System.out.println("Linea no valida");
+									}
+								}
+							}
+							catch (NumberFormatException nfe)
+							{
+								System.out.println("NumberFormatException: " + nfe.getMessage());
+								System.out.println("Linea no valida");
+							}
+							break;
+						}
+					}
+					System.out.println("---------------------------------------");
+					System.out.println(sentence.score);
+					System.out.println(sentence.text);
+				}
+				
+				return (List<Sentence>) sentence;
+			} else {
+				return null;
+			}
+
 		} catch (IOException e) {
 			System.out.println("Wrong file name. File " + filename + " can not be open.");
+			return null;
 		}
-
-		for(int i=0;i<list.size();i++) {
-			System.out.println(list.get(i));
-			sentence.score = i;
-			sentence.text = list.get(i);
-		}
-
-		return (List<Sentence>) sentence;
 	}
 
 	/*
--- src/test/java/AnalyzerTest.java
+++ src/test/java/AnalyzerTest.java
@@ -2,53 +2,33 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import org.junit.Test;
+import sun.misc.IOUtils;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AnalyzerTest {
     Analyzer reader = new Analyzer();
-    File archivoVacio;
-    File archivo;
-    String lineasArchivo = "2 Web delopment";
-
-
-    @BeforeClass
-    public void iniciarArchivos() throws IOException {
-        archivo = new File("archivo.txt");
-        archivoVacio = new File("archivoVacio.txt");
-        FileWriter writer = new FileWriter(archivo);
-        writer.write(lineasArchivo);
-        writer.close();
-        archivo.createNewFile();
-        archivoVacio.createNewFile();
-    }
-
-    @AfterClass
-    public void eliminarArchivos() {
-        archivo.delete();
-        archivoVacio.delete();
-    }
+    String lineasArchivo = "1 Web development";
 
     @Test
     public void leerArchivoCorrecto() {
-        ArrayList<String> strings = new ArrayList<>();
-        strings.add(lineasArchivo);
+        List<String> strings = Arrays.asList(lineasArchivo);
 
         Assert.assertTrue(reader.readFile("archivo.txt").equals(strings));
     }
 
     @Test
     public void leerArchivoVacio() {
-        ArrayList<String> strings = new ArrayList<>();
-
-        Assert.assertNotNull(reader.readFile("archivoVacio.txt"));
+         Assert.assertNull(reader.readFile("archivoVacio.txt"));
     }
 
     @Test
     public void leerArchivoNoExiste() throws IOException {
-        reader.readFile("archivoNoExiste.txt");
+         Assert.assertNull(reader.readFile("archivoNoExiste.txt"));
     }
 }
